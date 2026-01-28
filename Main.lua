7f3d102357bb2722ce9a4dc666b024b2924d907d
amabar49-boop <amabar49@gmail.com>
2026-01-28 18:27:04 +0700

    Main.lua

--- Main.lua
+++ Main.lua
@@ -2,5 +2,5 @@ print("Main GUI Loaded")
 
 -- contoh load module lain
 loadstring(game:HttpGet(
-    "https://raw.githubusercontent.com/silver-hub/REPO/main/fishit.lua"
+    "https://raw.githubusercontent.com/silver-hub/amabar49-boop/main/fishit.lua"
 ))()
