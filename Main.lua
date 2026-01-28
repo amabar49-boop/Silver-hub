a1c309529ac0184c5a46038cf39d7202836c905e
amabar49-boop <amabar49@gmail.com>
2026-01-28 18:37:39 +0700

    Update and rename Main.lua to main.lua

--- Main.lua
+++
@@ -1,6 +0,0 @@
-print("Main GUI Loaded")
-
--- contoh load module lain
-loadstring(game:HttpGet(
-    "https://raw.githubusercontent.com/silver-hub/amabar49-boop/main/fishit.lua"
-))()
---
+++ main.lua
@@ -0,0 +1,4 @@
+print("Main GUI Loaded")
+
+-- contoh load module lain
+loadstring(game:HttpGet("https://raw.githubusercontent.com/amabar49-boop/Silver-hub/main/Loader.lua"))()
