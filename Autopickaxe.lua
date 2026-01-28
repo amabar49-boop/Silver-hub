507866d43f4fe15bc1bca68c49f6b85bd0b2add4
amabar49-boop <amabar49@gmail.com>
2026-01-28 18:28:41 +0700

    Autopickaxe.lua

---
+++ Autopickaxe.lua
@@ -0,0 +1,29 @@
+print("Fish It Module Loaded")
+
+-- taruh autofarm / auto fish di sini
+--[[ 
+  Simple Loader
+  Author: You
+--]]
+
+local repo = "https://raw.githubusercontent.com/USERNAME/REPO/main/"
+
+local function load(file)
+    local success, err = pcall(function()
+        loadstring(game:HttpGet(repo .. file))()
+    end)
+
+    if not success then
+        warn("Failed to load:", file)
+        warn(err)
+    end
+end
+
+-- Anti double execute
+if getgenv()._MY_HUB_LOADED then
+    return
+end
+getgenv()._MY_HUB_LOADED = true
+
+-- Load main GUI
+load("main.lua")
